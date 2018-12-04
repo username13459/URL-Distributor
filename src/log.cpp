5457f20bca272876f18e9111c2d24e544e793b60
Z <username13459@gmail.com>
2018-12-04 13:17:58 -0500

    Created a log + logging handler

---
+++ src/log.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <string> 
+
+using std::string;
+using std::cout;
+using std::endl;
+
+#include "zlib/varTypes.h"
+
+#include "log.h"
+
+
+
+namespace progLog
+{
+	//The prefix and postfix for the log file name, to be placed around the timestamp the log was created
+	std::string logFilePrefix = "log";
+	std::string logFilePostfix = ".txt";
+
+	//The ofstream for the log
+	std::ofstream logOut;
+
+	bool fileOpen = false;
+
+	std::mutex writePerms;
+
+	void startLog()
+	{
+		var::longTime now = var::longTime::now();
+		string logName = logFilePrefix + now.getYMD() + "," + now.getHMS() + logFilePostfix;
+
+		try {
+			logOut.open(logName);
+			fileOpen = true;
+		}
+		catch(...)
+		{
+			cout << "UNABLE TO START LOG!" << endl;
+			throw "Unable to start log!";
+		}
+		
+		write("Log started successfully!");
+	}
+
+	void closeLog()
+	{
+		write("Log closed.");
+		writePerms.lock();
+		logOut.close();
+		fileOpen = false;
+		writePerms.unlock();
+	}
+
+	void write(string message)
+	{
+		var::longTime now = var::longTime::now();
+		writePerms.lock();
+
+		cout << now.getYMD() << ',' << now.getHMS() << " | " << message << endl;
+
+		if(fileOpen)
+		{
+
+			try
+			{
+
+				logOut << now.getYMD() << ',' << now.getHMS() << " | " << message << endl;
+				logOut.flush();
+			}
+			catch(...)
+			{
+				cout << "<<CRITICAL>> Unknown exception when writing to log!" << endl;
+			}
+		}
+		else cout << "<<CRITICAL>> Log not opened!" << endl;
+
+		writePerms.unlock();
+	}
+
+}
---
+++ src/log.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace progLog
+{
+	//Prepares for logging, opening the log file and etc.
+	void startLog();
+
+	//Writes a message to the log, with the timestamp appended to the front
+	void write(string message);
+
+	//Closes the current log
+	void closeLog();
+}
