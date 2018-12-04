9a52777678ab50ca5e0170fc3d4ab68259f8b93e
Z <username13459@gmail.com>
2018-12-04 13:19:54 -0500

    Some testing bits to the source, + updated the todo

--- src/Source.cpp
+++ src/Source.cpp
@@ -6,6 +6,7 @@ using namespace zlib::conv;
 
 #include "types.h"
 #include "db.h"
+#include "log.h"
 
 using std::cin;
 using std::cout;
@@ -29,16 +30,31 @@ void printIndexPair(blogIndexPair pair)
 	std::cout << pair.first << ", " << pair.second << std::endl;
 }
 
+
+
+void loadEverything()
+{
+	progLog::startLog();
+	blogDB::loadDB();
+}
+
+void writeAndCloseEverything()
+{
+	progLog::closeLog();
+}
+
 int main(int argc, char * argv[])
 {
-	blogDB::loadDB();
+	loadEverything();
 
-	cout << "blog size:" << sizeof(types::blog) << endl;
+	progLog::write("blog size:" + conv::toString(sizeof(types::blog)));
 
 	printIndexPair(blogDB::getIndexPair(0));
 	printIndexPair(blogDB::getIndexPair(blogDB::maxArraySize));
 	printIndexPair(blogDB::getIndexPair(blogDB::maxArraySize + 1));
 	printIndexPair(blogDB::getIndexPair(blogDB::maxArraySize * 5 + 2));
+
+	writeAndCloseEverything();
 
 	cin.get();
 	return 0;
