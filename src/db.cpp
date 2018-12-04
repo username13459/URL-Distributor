c8d945b99cad3ca0821ec38267dba2abfd68e012
Z <username13459@gmail.com>
2018-12-04 13:18:48 -0500

    Did lots of work getting the DB up to speed

--- src/db.cpp
+++ src/db.cpp
@@ -1,14 +1,22 @@
+#include<mutex>
+
 
 #include "types.h"
-
+#include "log.h"
 #include "db.h"
 
 namespace blogDB
 {
+	//Mutex for write perms to the DB
+	std::mutex dbWrite;
+
 	//An array pointing to arrays, which themselves contain blogs.  The backbone of the DB
 	blog ** blogArrays = NULL;
 	//The length of blogArrays
-	int blogArraysLen = 0;
+	//int blogArraysLen = 0;
+	//The number of blogs in the DB
+	blogIndex totalNumBlogs = 0;
+
 
 
 	//Separates a raw index into a major and minor index (which each correspond to a level in the array tree)
@@ -19,45 +27,160 @@ namespace blogDB
 
 	
 	//Returns a reference to the blog at 'index'
-	blog & getBlogRef(blogIndexPair index)
+	//If createMode is true, the db will create missing arrays as it goes, and disregard accessing positions past the end of the current db
+	blog & getBlogRef(blogIndexPair index, bool createMode = false)
 	{
-		blogIndex rawLen = index.first * maxArraySize + index.second;
+		if(!createMode)
+		{
+			blogIndex rawLen = index.first * maxArraySize + index.second;
 
-		if (rawLen >= blogArraysLen) throw dbOutOfBoundsException(rawLen);
+			if(rawLen >= totalNumBlogs) throw dbOutOfBoundsException(rawLen);
 
-		if (blogArrays == NULL) throw dbNULLPointerException("DB ROOT NULL!!!!");
+		}
 
-		if (blogArrays[index.first] == NULL) throw dbNULLPointerException(index);
+		//This shouldn't EVER happen, so createMode won't override this
+		throw dbNULLPointerException("DB ROOT NULL!");
+
+
+		if(blogArrays[index.first] == NULL)
+		{
+			if(!createMode) throw dbNULLPointerException(index);
+			else blogArrays[index.first] = new blog[maxArraySize];
+		}
 
 		return blogArrays[index.first][index.second];
 	}
 
 	//Returns a reference to the blog at 'index'
-	blog & getBlogRef(blogIndex index)
+	blog & getBlogRef(blogIndex index, bool createMode = false)
 	{
-		return getBlogRef(getIndexPair(index));
+		return getBlogRef(getIndexPair(index), createMode);
 	}
 
-	
+	std::vector<blog> getBlogsToArchive(int numBlogs, types::user archiver)
+	{
+		std::vector<blogIndexPair> pairs = getBlogIndexesToArchive(numBlogs);
+
+		for(int i = 0; i < pairs.size(); i++)
+		{
+			getBlogRef(pairs[i]).addDownloader(archiver);
+		}
+	}
+
+	//Returns a vector contaning blog index-pairs, where the blogs contained have the least number of copies present in the DB
+	std::vector<blogIndexPair> getBlogIndexesToArchive(int numBlogs)
+	{
+		std::vector<blogIndexPair> ret;
+
+		ret.reserve(numBlogs);
+
+		try
+		{
+			//"Seed" the list of blogs to return
+			for(int i = 0; i < numBlogs; i++)
+			{
+				ret.push_back(getIndexPair(i));
+			}
+
+			//Compare the DB to our shortlist
+			{
+				//Start comparing the db after the blogs we just seeded our list with
+				for(blogIndex index = numBlogs; index < totalNumBlogs; index++)
+				{
+					//Check the current item in the DB against our list
+					for(int i = 0; i < ret.size(); i++)
+					{
+						if(getBlogRef(ret[i]).getNumCopies() < getBlogRef(index).getNumCopies())
+						{
+							ret[i] = getIndexPair(index);
+							break;
+						}
+					}
+
+					//Check if all the blogs in our batch haven't been archived (copies ==0).  If so, just return this list
+					{
+						bool allZero = true;
+						for(int j = 0; j < ret.size(); j++)
+						{
+
+							if(getBlogRef(ret[j]).getNumCopies() != 0)
+							{
+								allZero = false;
+								break;
+							}
+						}
+
+						if(allZero) return ret;
+					}
+
+
+				}
+			}
+
+
+
+
+		}
+		catch(dbOutOfBoundsException e)
+		{
+			//If we get here we assume we tried to get more blogs than exist in the DB
+		};
+
+		return ret;
+	}
 
 
 	void append(blog newBlog)
 	{
-		if (blogArraysLen == maxArraySize * maxArraySize) throw dbException("DB has reached max space!");
+		dbWrite.lock();
+		if (totalNumBlogs == maxArraySize * maxArraySize) throw dbException("DB has reached max space!");
 
-		getBlogRef(blogArraysLen) = newBlog;
-		blogArraysLen++;
+		getBlogRef(totalNumBlogs, true) = newBlog;
+		totalNumBlogs++;
+		dbWrite.unlock();
 	}
 
 
 	void loadDB()
 	{
-		blogArraysLen = 0;
+		dbWrite.lock();
+		totalNumBlogs = 0;
 
 		blogArrays = new blog*[maxArraySize];
+
+
+
+
+		dbWrite.unlock();
 	}
 
+	void sortDB()
+	{
+		//This works off a crappy bubble-sort algorithm, and should DEF be replaced later
+		progLog::write("Preparing to sort DB");
+		dbWrite.lock();
+		progLog::write("DB lock acquired.");
 
+		bool sorted = false;
+		long long unsigned numPasses = 0;
+		while(!sorted)
+		{
+			sorted = true;
+			for(blogIndex i = 0; i < totalNumBlogs - 1; i++)
+			{
+				if(getBlogRef(i).getNumCopies() > getBlogRef(i + 1).getNumCopies())
+				{
+					sorted = false;
+					std::swap(getBlogRef(i), getBlogRef(i + 1));
+				}
+			}
+			numPasses++;
+			if(numPasses % 100 == 0) progLog::write("DB Sort passes made: " + conv::toString(numPasses));
+		}
+
+		progLog::write("Sorting finished.");
+		dbWrite.unlock();
+	}
 
 
 }
--- src/db.h
+++ src/db.h
@@ -13,7 +13,7 @@ using types::blog;
 typedef long int blogIndex;
 typedef std::pair<int, int> blogIndexPair;
 
-struct dbException
+struct dbException : types::Exception
 {
 	dbException() {}
 	dbException(string details) { this->details = details; }
@@ -35,15 +35,32 @@ struct dbNULLPointerException : dbException
 	dbNULLPointerException(blogIndexPair indexRequested) { this->details = "" + conv::toString(indexRequested.first) + "," + conv::toString(indexRequested.second); }
 };
 
+//Thrown when 'numBlogs' passed to getBlogsToArchive is a bad value
+struct dbBadGetToArchiveException : dbException
+{
+	dbBadGetToArchiveException() {}
+	dbBadGetToArchiveException(string details) { this->details = details; }
+};
+
 namespace blogDB
 {	
 	static const long int maxArraySize = 1'000'000;
 
+	//Converts an index into an index pair
 	blogIndexPair getIndexPair(blogIndex index);
 
+	//Returns 'numBlogs' (or fewer) blogs to archive)
+	std::vector<blog> getBlogsToArchive(int numBlogs);
+
+	//Appends a blog to the dp
 	void append(blog newBlog);
 
+	//Loads the DB
 	void loadDB();
+
+	//Sorts the DB from least-archived to most-archived
+	//THIS IS RESOURCE INTENSIVE, AND WILL LOCK THE DB FOR SOME TIME
+	void sortDB();
 
 
 
